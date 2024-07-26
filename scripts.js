1c7ce2bfc90af68282f6082f26107aea7127fa17
Your Name <alessanrospedroso@outlook.com>
2024-07-26 17:51:07 -0300

    --

--- scripts.js
+++ scripts.js
@@ -4,9 +4,18 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert('O valo minimo tem que ser MENOR que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    }
+
+    console.log(min,max)
+
     
-    alert(result)
+    
+    
 }
 
 button.addEventListener('click',generateNumber)
